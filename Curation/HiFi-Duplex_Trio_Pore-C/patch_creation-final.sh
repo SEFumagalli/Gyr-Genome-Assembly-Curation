eb7782efd461c5853e1cc28af46e7b9506b05be4
Sarah Fumagalli <sefumagal@gmail.com>
2026-06-16 16:32:35 -0500

    updated patch_creation-final

--- Curation/HiFi-Duplex_Trio_Pore-C/patch_creation-final.sh
+++ Curation/HiFi-Duplex_Trio_Pore-C/patch_creation-final.sh
@@ -1,167 +1,167 @@
 #created by Sarah E. Fumagalli
 
 
-patch_dir="/90daydata/ruminant_t2t/Gyr/assembly/verkko2.2.1_hifi-duplex_tporec/8-manualResolution/assembly_patch"
-verkko_dir="/90daydata/ruminant_t2t/Gyr/assembly/verkko2.2.1_hifi-duplex_tporec"
-verkko_fillet_dir="/90daydata/ruminant_t2t/Gyr/assembly/verkko2.2.1_hifi-duplex_tporec_verkko_fillet"
+patch_dir="/assembly/verkko2.2.1_hifi-duplex_tporec/8-manualResolution/final_patch"
+verkko_dir="/assembly/verkko2.2.1_hifi-duplex_tporec"
+verkko_fillet_dir="/assembly/verkko2.2.1_hifi-duplex_tporec_verkko_fillet"
 
 
 ## ------------------------------------------------------------------------------------------------------------------------------------------------
-##
-##
-## Final patch
-##
-##
-## ------------------------------------------------------------------------------------------------------------------------------------------------
 
 
-cd $patch_dir
-cd final_patch
+1. Concatenate all telomere and rDNA patches into a single file 
 
-## Concatenate all patches into a single file 
+    cd $patch_dir
 
-	cat ../rDNA-patches/utig4-439_chr11.hap2_RC_line1.patch.gaf ../telomere_alignments/rDNA_telo/utig4-393_RC_chr11.hap2_RC_line1.patch.gaf ../rDNA-patches/utig4-438_chr11.hap1_line1.patch.gaf ../telomere_alignments/rDNA_telo/utig4-1331_RC_chr11.hap1_line1.patch.gaf ../rDNA-patches/utig4-278_chr3.hap1_RC_line1.patch.gaf ../telomere_alignments/rDNA_telo/utig4-251_chr3.hap1_RC_line2.patch.gaf ../rDNA-patches/utig4-918_chr3.hap2_line1.patch.gaf ../telomere_alignments/rDNA_telo/utig4-70_RC_chr3.hap2_line1.patch.gaf ../rDNA-patches/utig4-982_chr2.hap2_RC_line1.patch.gaf ../telomere_alignments/rDNA_telo/utig4-456_chr2.hap2_RC_line2.patch.gaf ../rDNA-patches/utig4-376_chr25.hap1_2_RC_line2.patch.gaf ../telomere_alignments/rDNA_telo/utig4-2334_chr25.hap1_2_RC_line1.patch.gaf ../rDNA-patches/utig4-607_chr2.hap1_line1.patch.gaf ../telomere_alignments/rDNA_telo/utig4-356_RC_chr2.hap1_line2.patch.gaf ../telomere_alignments/telo_1854/utig4-1854_utig4-1854_line1.patch.gaf ../telomere_alignments/telo_1854/utig4-2375_utig4-1854_line1.patch.gaf ../telomere_alignments/telo_2468/utig4-2468_hap2_346_line1.patch.gaf ../telomere_alignments/telo_2468/hap2_346_hap2_346_line1.patch.gaf > patchAlign.gaf
+	cat ../rDNA/utig4-439_chr11.hap2_RC_line1.patch.gaf ../telomeres/utig4-393_RC_chr11.hap2_RC_line1.patch.gaf ../rDNA/utig4-438_chr11.hap1_line1.patch.gaf ../telomeres/utig4-1331_RC_chr11.hap1_line1.patch.gaf ../telomeres/telo_1854/utig4-1854_utig4-1854_line1.patch.gaf ../telomeres/telo_1854/utig4-2375_utig4-1854_line1.patch.gaf > patchAlign.gaf
 
+    
+    Make sure there are pairs of alignments with the same name on the left side of the patchAlign.gaf
+    
+        - Example
 
-	**Make sure there are pairs of alignments with the same name on the left side of the patchAlign.gaf
-
-
-
-## Remove lines that look spurious in the rDNA patches
-
-	micromamba activate verkko-v2.2.1
-
-	/project/cattle_genome_assemblies/packages/micromamba/envs/verkko-v2.2.1/lib/verkko/scripts/insert_aln_gaps.py ../assembly.homopolymer-compressed.gfa ../rDNA-patches/rDNA_patchAlign.gaf 1 100000 patch.nogap.gaf patch.gaf gapmanual y > patch.gfa
+            tangle0_heavy_path      2918278 15      8687    +       >utig4-439      5616868 5608223 5616851 8511    8672    60      tp:A:P  s1:i:8501       dv:f:   id:f:1
+            tangle0_heavy_path      2918278 2782124 2912763 +       <utig4-393      155286  9       129297  122644  131376  60      tp:A:P  s1:i:121972     dv:f:   id:f:1
+            tangle1_heavy_path      3107320 15      15023   +       >utig4-438      5724960 5709947 5724951 14733   15012   60      tp:A:P  s1:i:14730      dv:f:   id:f:1
+            tangle1_heavy_path      3107320 3040503 3107316 +       >utig4-1331     77961   18      66979   64179   67233   60      tp:A:P  s1:i:64002      dv:f:   id:f:1
 
 
 
+2. Remove lines that look spurious in the rDNA and telomere patches
 
+    This script can be found on the Verkko github   https://github.com/marbl/verkko
 
-## Add the necessary info for patches to respective files
+        	micromamba activate verkko-v2.2.1
 
-#combine patches with previous alignments
-cp ../../../6-layoutContigs/combined-alignments.gaf ./
-cat patchAlign.gaf >> combined-alignments.gaf
+        	insert_aln_gaps.py ../assembly.homopolymer-compressed.gfa patchAlign.gaf 1 100000 patch.nogap.gaf patch.gaf gapmanual y > patch.gfa
 
-#combine patch edges with previous edges 
-cp ../../../6-layoutContigs/combined-edges.gfa ./
-cat patch.gfa | grep '^L' |grep gap >> combined-edges.gfa
-
-#combine node lengths to previous file
-ln -s ../../../6-layoutContigs/combined-nodemap.txt
-cp ../../../6-layoutContigs/nodelens.txt ./
-cat patch.gfa | grep gap | awk 'BEGIN { FS="[ \t]+"; OFS="\t"; } ($1 == "S") && ($3 != "*") { print $2, length($3); }' >> nodelens.txt
-
-#combine subset to previous file
-cp ../../../7-consensus/ont_subset.fasta.gz ./
-
-#copy in all rDNA and regular patch fastas
-cp /90daydata/ruminant_t2t/Gyr/assembly/verkko2.2.1_hifi-duplex_tporec/8-manualResolution/verkko2.2.1_hifi-duplex_tporec_patch/rDNA-patches/chr11.hap2.patch.fa .
-cp /90daydata/ruminant_t2t/Gyr/assembly/verkko2.2.1_hifi-duplex_tporec/8-manualResolution/verkko2.2.1_hifi-duplex_tporec_patch/rDNA-patches/chr11.hap1.patch.fa .
-cp /90daydata/ruminant_t2t/Gyr/assembly/verkko2.2.1_hifi-duplex_tporec/8-manualResolution/verkko2.2.1_hifi-duplex_tporec_patch/rDNA-patches/chr3.hap1.patch.fa .
-cp /90daydata/ruminant_t2t/Gyr/assembly/verkko2.2.1_hifi-duplex_tporec/8-manualResolution/verkko2.2.1_hifi-duplex_tporec_patch/rDNA-patches/chr3.hap2.patch.fa .
-cp /90daydata/ruminant_t2t/Gyr/assembly/verkko2.2.1_hifi-duplex_tporec/8-manualResolution/verkko2.2.1_hifi-duplex_tporec_patch/rDNA-patches/chr2.hap2.patch.fa .
-cp /90daydata/ruminant_t2t/Gyr/assembly/verkko2.2.1_hifi-duplex_tporec/8-manualResolution/verkko2.2.1_hifi-duplex_tporec_patch/rDNA-patches/chr25.hap1_2.patch.fa .
-cp /90daydata/ruminant_t2t/Gyr/assembly/verkko2.2.1_hifi-duplex_tporec/8-manualResolution/verkko2.2.1_hifi-duplex_tporec_patch/rDNA-patches/chr2.hap1.patch.fa .
-cp /90daydata/ruminant_t2t/Gyr/assembly/verkko2.2.1_hifi-duplex_tporec/8-manualResolution/verkko2.2.1_hifi-duplex_tporec_patch/telomere_alignments/telo_1854/utig4-1349-nonhpc.fasta
-cp /90daydata/ruminant_t2t/Gyr/assembly/verkko2.2.1_hifi-duplex_tporec/8-manualResolution/verkko2.2.1_hifi-duplex_tporec_patch/telomere_alignments/telo_2468/need nonhpc patch fasta
-
-#concatenate patches
-cat chr11.hap2.patch.fa | gzip -c >> ont_subset.fasta.gz
-cat chr11.hap1.patch.fa | gzip -c >> ont_subset.fasta.gz
-cat chr3.hap1.patch.fa | gzip -c >> ont_subset.fasta.gz
-cat chr3.hap1.patch.fa | gzip -c >> ont_subset.fasta.gz
-cat chr2.hap2.patch.fa | gzip -c >> ont_subset.fasta.gz
-cat chr25.hap1_2.patch.fa | gzip -c >> ont_subset.fasta.gz
-cat chr2.hap1.patch.fa | gzip -c >> ont_subset.fasta.gz
-cat utig4-1349-nonhpc.fasta | gzip -c >> ont_subset.fasta.gz
-cat utig4-?-nonhpc.fasta | gzip -c >> ont_subset.fasta.gz
-
-micromamba activate seqtk
-seqtk gc ont_subset.fasta.gz |awk '{print $1}'|sort |uniq > ont_subset.id
-
-
-#copy gap fixes and other needed files 
-cp ../gaps/gap.paths.gaf .
-cp ../../../6-layoutContigs/unitig-popped.layout .
-cp ../../../6-layoutContigs/unitig-popped.layout.scfmap .
-
-
-# Confirm new assembly layout & paths are valid
-salloc --account=cattle_genome_assemblies -p priority -q agil -c 96 --mem-per-cpu=3968 --time=1-00:00:00
-
-micromamba activate verkko-v2.2.1
-
-/project/cattle_genome_assemblies/packages/micromamba/envs/verkko-v2.2.1/lib/verkko/scripts/get_layout_from_mbg.py combined-nodemap.txt combined-edges.gfa combined-alignments.gaf gap.paths.gaf nodelens.txt unitig-popped.layout unitig-popped.layout.scfmap
+        
+        - if Verkko does not accept your patch, it will not produce a result 
+        
+        - the window_size can be increased (100000) but within reason
 
 
 
+3. Add the necessary info for patches to respective files
+
+    Combine patches with previous alignments
+    
+        cp ../../../6-layoutContigs/combined-alignments.gaf ./
+
+        cat patchAlign.gaf >> combined-alignments.gaf
 
 
-# Set up verkko_final_asm folder
-mkdir verkko_final_asm
-final_verkko_dir="/90daydata/ruminant_t2t/Gyr/assembly/verkko2.2.1_hifi-duplex_tporec_final_asm"
-cd $final_verkko_dir
+    Combine patch edges with previous edges 
+    
+        cp ../../../6-layoutContigs/combined-edges.gfa ./
 
-ln -s  ../$verkko_dir/1-buildGraph/
-ln -s  ../$verkko_dir/2-processGraph/
-ln -s  ../$verkko_dir/3-align
-ln -s  ../$verkko_dir/3-alignTips/
-ln -s  ../$verkko_dir/4-processONT/
-ln -s  ../$verkko_dir/5-untip/
-
-mkdir 6-layoutContigs
-cd 6-layoutContigs
-ln -s ../../combined-nodemap.txt
-ln -s ../../combined-edges.gfa
-ln -s ../../combined-alignments.gaf
-ln -s ../../nodelens.txt
-
-cd ..
-
-This folder does not exist
-mkdir 6-rukki
-cd 6-rukki
-ln -s $verkko_dir/6-rukki/unitig-unrolled-unitig-unrolled-popped-unitig-normal-connected-tip.noseq.gfa
-ln -s $verkko_dir/6-rukki/unitig-unrolled-unitig-unrolled-popped-unitig-normal-connected-tip.colors.csv
-ln -s ../../new.paths.gaf unitig-unrolled-unitig-unrolled-popped-unitig-normal-connected-tip.paths.gaf
-ln -s ../../new.paths.gaf unitig-unrolled-unitig-unrolled-popped-unitig-normal-connected-tip.paths.tsv
-
-cd ../
-mkdir 7-consensus
-cd 7-consensus
-ln -s ../../ont_subset.id
-ln -s ../../ont_subset.fasta.gz
+        cat patch.gfa | grep '^L' |grep gap >> combined-edges.gfa
 
 
-# Rerun Verkko w/ patches
-cd $new_verkko_dir
-ln -s /project/ruminant_t2t/Gyr/assembly/duplex-hifi
-ln -s /project/ruminant_t2t/Gyr/assembly/combined
-ln -s /project/ruminant_t2t/existing_NCBI_references/Cattle/ARS-UCD2.0_chr.fasta
-ln -s /project/ruminant_t2t/Gyr/new_assemblies/Cattle_rDNA.fasta
-ln -s /project/ruminant_t2t/Gyr/new_assemblies/Cattle_MT.fasta
-ln -s /project/ruminant_t2t/Gyr/PoreC
+    Combine node lengths to previous file
+    
+        ln -s ../../../6-layoutContigs/combined-nodemap.txt
+        
+        cp ../../../6-layoutContigs/nodelens.txt ./
+        
+        cat patch.gfa | grep gap | awk 'BEGIN { FS="[ \t]+"; OFS="\t"; } ($1 == "S") && ($3 != "*") { print $2, length($3); }' >> nodelens.txt
+        
+
+    Combine subset to previous file
+    
+        cp ../../../7-consensus/ont_subset.fasta.gz ./
+        
+
+    Copy in all rDNA and regular patch fastas
+    
+        cp /assembly/verkko2.2.1_hifi-duplex_tporec/8-manualResolution/rDNA/chr11.hap2.patch.fa .
+
+        cp /assembly/verkko2.2.1_hifi-duplex_tporec/8-manualResolution/telomeres/telo_1854/utig4-1349.fasta
+        
+
+    Concatenate patches
+    
+        cat chr11.hap2.patch.fa | gzip -c >> ont_subset.fasta.gz
+
+        cat utig4-1349.fasta | gzip -c >> ont_subset.fasta.gz
+
+        seqtk gc ont_subset.fasta.gz |awk '{print $1}'|sort |uniq > ont_subset.id
 
 
-micromamba activate verkko-v2.2.1
-verkko --version
+    Copy gap patches and related files
+    
+        cp ../gaps/gap.paths.gaf .
+        
+        cp ../../../6-layoutContigs/unitig-popped.layout .
 
-verkko --slurm --snakeopts "--touch" -d verkko2.2.1_hifi-duplex_tporec_relaunch --unitig-abundance 4 --red-run 8 40 8 --hifi duplex_hifi/*.fastq.gz --nano combined/*.fastq.gz --screen Cattle_MT Cattle_MT.fasta --screen Cattle_rDNA Cattle_rDNA.fasta --porec PoreC/AllPoreC.gt10q.fastq.gz > dev/null 2>&1
-
-verkko --slurm --snakeopts "--dry-run" -d verkko2.2.1_hifi-duplex_tporec_relaunch --unitig-abundance 4 --red-run 8 40 8 --hifi duplex_hifi/*.fastq.gz --nano combined/*.fastq.gz --screen Cattle_MT Cattle_MT.fasta --screen Cattle_rDNA Cattle_rDNA.fasta --porec PoreC/AllPoreC.gt10q.fastq.gz
-
-sbatch --parsable --cpus-per-task=2 --mem 50g --output verkko2.2.1_hifi-duplex_tporec_relaunch.out launch_verkko_final_asm.sh
+        cp ../../../6-layoutContigs/unitig-popped.layout.scfmap .
 
 
-# ./launch_verkko_final_asm.sh
-# ----------------------------
-#!/bin/bash -l
-micromamba activate verkko-v2.2.1
-verkko --version
+    Confirm gap patches are valid
 
-verkko --slurm -d verkko2.2.1_hifi-duplex_tporec_relaunch --unitig-abundance 4 --red-run 8 40 8 --hifi duplex_hifi/*.fastq.gz --nano combined/*.fastq.gz --screen Cattle_MT Cattle_MT.fasta --screen Cattle_rDNA Cattle_rDNA.fasta --porec PoreC/AllPoreC.gt10q.fastq.gz
-# -----------------------------
+        This script can be found on the Verkko github   https://github.com/marbl/verkko
+
+            micromamba activate verkko-v2.2.1
+
+            get_layout_from_mbg.py combined-nodemap.txt combined-edges.gfa combined-alignments.gaf gap.paths.gaf nodelens.txt unitig-popped.layout unitig-popped.layout.scfmap
+
+            
+            - if there are no errors, gap patches are accepted by Verkko
+
+
+
+
+4. Set up relaunch Verkko folder
+
+    mkdir $verkko_dir/verkko_final_asm && cd $verkko_dir/verkko_final_asm
+
+    ln -s  ../$verkko_dir/1-buildGraph/
+    ln -s  ../$verkko_dir/2-processGraph/
+    ln -s  ../$verkko_dir/3-align
+    ln -s  ../$verkko_dir/3-alignTips/
+    ln -s  ../$verkko_dir/4-processONT/
+    ln -s  ../$verkko_dir/5-untip/
+    
+    mkdir 6-layoutContigs && cd 6-layoutContigs
+    
+    ln -s ../../combined-nodemap.txt
+    ln -s ../../combined-edges.gfa
+    ln -s ../../combined-alignments.gaf
+    ln -s ../../nodelens.txt
+    
+    cd ..
+    
+    mkdir 6-rukki && cd 6-rukki (if the assembly was created without trio data - look for similar files in 8-hicPipeline)
+    
+    ln -s $verkko_dir/6-rukki/unitig-unrolled-unitig-unrolled-popped-unitig-normal-connected-tip.noseq.gfa
+    ln -s $verkko_dir/6-rukki/unitig-unrolled-unitig-unrolled-popped-unitig-normal-connected-tip.colors.csv
+    ln -s ../../gap.paths.gaf unitig-unrolled-unitig-unrolled-popped-unitig-normal-connected-tip.paths.gaf
+    ln -s ../../gap.paths.gaf unitig-unrolled-unitig-unrolled-popped-unitig-normal-connected-tip.paths.tsv
+    
+    cd ../
+    
+    mkdir 7-consensus && cd 7-consensus
+    
+    ln -s ../../ont_subset.id
+    ln -s ../../ont_subset.fasta.gz
+    
+    cd ../
+    cd ../
+
+
+5. Relaunch Verkko
+
+    Use --snakeopts "--touch" and "--dry-run" 
+
+    micromamba activate verkko-v2.2.1
+
+    verkko --slurm -d verkko_final_asm --unitig-abundance 4 --red-run 8 40 8 \
+        --hifi <*.fastq.gz> \
+        --nano <*.fastq.gz> \
+        --screen <mito_file_name> <reference_mito.fasta> \
+        --screen <rDNA_file_name> <reference_rDNA.fasta> \
+        --porec/hic/hapmers <*.fastq.gz>
+
 
 
 
